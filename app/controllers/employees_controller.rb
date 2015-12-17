80727c1997cd208376d0928e821309a43894bd6b
Peter Jang <peter.x.jang@gmail.com>
2015-12-16 19:00:22 -0600

    added edit, update, and delete actions

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -22,4 +22,26 @@ class EmployeesController < ApplicationController
     ).body
     redirect_to "/employees/#{@employee['id']}"
   end
+
+  def edit
+    @employee = Unirest.get("http://localhost:3000/employees/#{params[:id]}.json").body
+  end
+
+  def update
+    @employee = Unirest.patch(
+      "http://localhost:3000/employees/#{params['id']}.json",
+      headers: { "Accept" => "application/json" }, 
+      parameters: {
+        first_name: params[:first_name],
+        last_name: params[:last_name],
+        email: params[:email]
+      }
+    ).body
+    redirect_to "/employees/#{@employee['id']}"
+  end
+
+  def destroy
+    Unirest.delete("http://localhost:3000/employees/#{params['id']}.json")
+    redirect_to "/employees"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get '/employees/new' => 'employees#new'
   get '/employees/:id' => 'employees#show'
   post '/employees' => 'employees#create'
+  get '/employees/:id/edit' => 'employees#edit'
+  patch '/employees/:id' => 'employees#update'
+  delete '/employees/:id' => 'employees#destroy'
 end
